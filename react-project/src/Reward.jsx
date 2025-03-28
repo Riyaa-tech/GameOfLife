dbe6882a969efc82269612c2738a538c25b88a60
Riyaa-tech <riyaasonawane@gmail.com>
2025-03-28 19:26:13 +0530

    Update Reward.jsx

--- react-project/src/Reward.jsx
+++ react-project/src/Reward.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import "./GamePage.css";
 
-const Payday = ({
+const Reward = ({
+  activePlayer,
+  player1,
+  player2,
   setPlayer1,
   setPlayer2,
   loc1,
   loc2,
   loc_cell,
+  message,
   rewardAmount,
 }) => {
   const [rewardTriggeredP1, setrewardTriggeredP1] = useState(false);
@@ -14,27 +18,36 @@ const Payday = ({
 
   useEffect(() => {
     // Trigger payday for Player 1
-    if (!rewardTriggeredP1 && loc1 == loc_cell) {
+    if (!rewardTriggeredP1 && loc1 == loc_cell && activePlayer === player1) {
       setPlayer1((prev) => ({ ...prev, amount: prev.amount + rewardAmount }));
       setrewardTriggeredP1(true);
-      console.log("reward triggered for Player 1");
+      /*
+      alert("reward triggered for Player 1");
+      */
     }
 
     // Trigger payday for Player 2
-    if (!rewardTriggeredP2 && loc2 == loc_cell) {
+    if (!rewardTriggeredP2 && loc2 == loc_cell && activePlayer === player2) {
       setPlayer2((prev) => ({ ...prev, amount: prev.amount + rewardAmount }));
       setrewardTriggeredP2(true);
-      console.log("reward triggered for Player 2");
+      /*
+      alert("reward triggered for Player 2");
+      */
     }
   }, [
-    loc1,
-    loc2,
-    rewardTriggeredP1,
-    rewardTriggeredP2,
-    loc_cell,
+    activePlayer,
+    player1,
+    player2,
+
     setPlayer1,
     setPlayer2,
+    loc1,
+    loc2,
+    loc_cell,
+    message,
     rewardAmount,
+    rewardTriggeredP1,
+    rewardTriggeredP2,
   ]);
 
   // Reset payday trigger when players move away from payday cell
@@ -43,13 +56,7 @@ const Payday = ({
     if (loc2 < loc_cell) setrewardTriggeredP2(false);
   }, [loc1, loc2, loc_cell]);
 
-  return (
-    <div>
-      <div id="RewardButton" className="cell-v cell-30 cell-corner-bottom-left">
-        Reward
-      </div>
-    </div>
-  );
+  return <div className="cellFill">{message}</div>;
 };
 
-export default Payday;
+export default Reward;
