7711eba6b3aa6c23feaae556c0fd996ed28c53eb
Riyaa-tech <riyaasonawane@gmail.com>
2025-03-28 19:48:08 +0530

    Update main.jsx

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
+import "https://github.com/Riyaa-tech/GameOfLife/blob/main/react-project/src/index.css";
+import App from "https://github.com/Riyaa-tech/GameOfLife/blob/main/react-project/src/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
